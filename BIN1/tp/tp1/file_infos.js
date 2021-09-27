11b2a77ba91c97f50e27b858c3805d4890d4132e
Karl MARQUES <kmarques@vetixy.com>
2021-09-27 09:30:57 +0200

    BIN1: add TP

--- BIN1/tp/tp1/file_infos.js
+++ BIN1/tp/tp1/file_infos.js
@@ -2,7 +2,7 @@
 // Le but du script étant de lire le fichier, détecter le nombre de lignes de données
 // et ressortir les différents nom de colonnes
 // Format attendu:
-//      Nombre de lignes détecté: 10
+//      Nombre de lignes détecté: 4
 //      Nombre de colonnes détecté: 5
 //      Colonnes: id, nom, address, dob, email
 
