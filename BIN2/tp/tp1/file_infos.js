eebaa2f5d502d291ca1af0554606d93fb85c7f14
Karl MARQUES <kmarques@vetixy.com>
2021-09-27 15:25:53 +0200

    BIN2: tp1 correction

--- BIN2/tp/tp1/file_infos.js
+++ BIN2/tp/tp1/file_infos.js
@@ -7,5 +7,52 @@
 //      Nombre de colonnes détecté: 5
 //      Colonnes: id, nom, address, dob, email
 const fs = require("fs/promises");
+const { constants } = require("fs");
+const path = require("path");
 
-const filePath = "./BIN2/tp/tp1/test.csv";
+const [filePath = "", extension = ""] = process.argv.slice(2);
+
+fs.access(filePath, constants.R_OK)
+  .then(() => fs.readFile(filePath))
+  .then((data) => {
+    //const extension = "." + filePath.split(".").pop();
+    const extensionF = extension
+      ? "." + extension.toLowerCase()
+      : path.extname(filePath);
+
+    switch (extensionF) {
+      case ".csv":
+        return parseCSV(data);
+      case ".json":
+        return parseJSON(data);
+    }
+  })
+  .then((data) => {
+    console.log(`Nombre de lignes détecté: ${data.nbLines}`);
+    console.log(`Nombre de colonnes détecté: ${data.nbColumns}`);
+    console.log(`Colonnes: ${data.headers.join(", ")}`);
+  })
+  .catch((e) => console.error(e));
+
+function parseCSV(buffer) {
+  const data = buffer.toString();
+  const lines = data.split("\n");
+  const headers = lines[0].split(";");
+
+  return {
+    nbLines: lines.length - 1,
+    nbColumns: headers.length,
+    headers,
+  };
+}
+
+function parseJSON(buffer) {
+  const lines = JSON.parse(buffer);
+  const headers = Object.keys(lines[0]);
+
+  return {
+    nbLines: lines.length,
+    nbColumns: headers.length,
+    headers,
+  };
+}
