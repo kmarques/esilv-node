b7cf801565aa7bd98f61ede376921b2fa1db29d4
Karl MARQUES <kmarques@vetixy.com>
2021-09-27 09:04:34 +0200

    BIN1: add TP

---
+++ BIN1/tp/tp1/file_infos.js
@@ -0,0 +1,9 @@
+// Lire un fichier donné en paramètre. Celui-ci peut être de format JSON, CSV.
+// Le but du script étant de lire le fichier, détecter le nombre de lignes de données
+// et ressortir les différents nom de colonnes
+// Format attendu:
+//      Nombre de lignes détecté: 10
+//      Nombre de colonnes détecté: 5
+//      Colonnes: id, nom, address, dob, email
+
+const file_path = "./test.csv";
---
+++ BIN1/utils/log_analyzer/analyser_promises.js
@@ -0,0 +1,16 @@
+const fs = require("fs/promises");
+
+const filePath = "./BIN1/utils/log_analyzer/access.log";
+
+fs.access(filePath, fs.constants.R_OK)
+  .then(() => fs.readFile(filePath))
+  .then((data) => {
+    const logs = data.toString();
+    return logs.split("\n").map(function (item) {
+      return item.match(
+        /(?<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2}) (?<verb>GET|POST|PUT|DELETE) (?<code>\d{3}) (?<link>.*)/
+      ).groups;
+    });
+  })
+  .then((data) => console.log(data))
+  .catch((e) => console.error(e));
