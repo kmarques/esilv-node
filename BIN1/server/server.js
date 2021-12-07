a0cc0b16ab79aff64463f5cfe0854a2077bc38d3
Karl MARQUES <kmarques@vetixy.com>
2021-12-07 11:09:55 +0100

    project subject

--- BIN1/server/server.js
+++ BIN1/server/server.js
@@ -5,6 +5,37 @@ const UserRouter = require("./routes/user");
 const PostRouter = require("./routes/post");
 //const generateRoutes = require("./routes/genericRouter");
 
+/**
+ * /users : Operations de collection sur des Users
+ * /users/:id : Operations d'item User
+ *
+ * /vehicules : Operations de collection sur des Vehicules
+ * /vehicules/:id : Operations d'item Vehicule
+ *
+ * GET récupère un ou plusieurs items
+ *  - /users
+ *      200 : OK
+ *  - /users/:id
+ *     200 : OK
+ *     404 : Not Found
+ *
+ * POST crée un item
+ *  - /users
+ *     201 : Created
+ *     400 : Bad Request
+ *
+ * PUT met à jour un item
+ *  - /users/:id
+ *    200 : OK
+ *    400 : Bad Request
+ *    404 : Not Found
+ *
+ * DELETE supprime un item
+ *  - /users/:id
+ *    204 : No Content
+ *    404 : Not Found
+ */
+
 //const Post = require("./models/Post");
 //const User = require("./models/User");
 
